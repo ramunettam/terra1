6cd9c3049b4ebaef837b53f4aa21ca3e5ad48b86
ramunettam <167076053+ramunettam@users.noreply.github.com>
2024-05-08 09:33:46 +0200

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "db" {
+  ami = "ami-090252cbe067a9e58"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+
+  tags = {
+    Name = "db"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "allowing SSH access"
+
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0 # from 0 to 0 means, opening all protocols
+        to_port          = 0
+        protocol         = "-1" # -1 all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_ssh"
+        CreatedBy = "ramu"
+    }
+}
